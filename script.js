c48cb49b99670835c7718c2e847d08db6b8958cf
Stefan-Serhii <stefan770@ukr.net>
2021-01-19 15:43:25 +0200

    homework 4

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function one(data){
+function outputData(data){
     console.log(data);
     document.querySelector('.package-name').innerHTML = data.name;
     document.querySelector('.prise').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
@@ -7,52 +7,33 @@ function one(data){
     document.querySelector('.features li:last-child').innerHTML = data.sys['country'];    
 };
 
-function two(id){
+function loadWetherAndOutput(id){
     fetch(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=2850bfc126a41dc9689aeb6ae8218480`)
-    .then(function(resp){return resp.json()} )
-    .then(one);
+        .then(function(resp){return resp.json()} )
+        .then(outputData);
 }
 
 document.querySelector('.city').onchange = () =>{
     let a = document.querySelector('.city').value;
+    let id = transformCityToId(a);
+    loadWetherAndOutput(id);
+}
 
+function transformCityToId(a){
     if(a == '2'){
-        fetch('https://api.openweathermap.org/data/2.5/weather?id=690548&appid=2850bfc126a41dc9689aeb6ae8218480')
-        .then(function(resp){return resp.json()} )
-        .then(one);
+        return 690548;
     }else if(a == '3'){
-        fetch('https://api.openweathermap.org/data/2.5/weather?id=689558&appid=2850bfc126a41dc9689aeb6ae8218480')
-        .then(function(resp){return resp.json()} )
-        .then(one)
+        return 689558;
     }else if(a == '1'){
-        fetch('https://api.openweathermap.org/data/2.5/weather?id=703448&appid=2850bfc126a41dc9689aeb6ae8218480')
-        .then(function(resp){return resp.json()} )
-        .then(one)
+        return 703448;
     }else if(a == '4'){
-        fetch('https://api.openweathermap.org/data/2.5/weather?id=705914&appid=2850bfc126a41dc9689aeb6ae8218480')
-        .then(function(resp){return resp.json()} )
-        .then(one)
+        return 705914;
     }else if(a == '5'){
-        fetch('https://api.openweathermap.org/data/2.5/weather?id=1510853&appid=2850bfc126a41dc9689aeb6ae8218480')
-        .then(function(resp){return resp.json()} )
-        .then(one)
+        return 1510853;
     }
 }
 
-fetch('https://api.openweathermap.org/data/2.5/weather?id=703448&appid=2850bfc126a41dc9689aeb6ae8218480') 
-    .then(function(resp){return resp.json()} )
-    .then(function(data){
-        console.log(data);
-        document.querySelector('.package-name').innerHTML = data.name;
-        document.querySelector('.prise').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-        document.querySelector('.desclaimer').innerHTML = data.weather[0]['description'];
-        document.querySelector('.features li:first-child').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-        document.querySelector('.features li:last-child').innerHTML = data.sys['country']; 
-    })
-    .catch(function(error){
-        console.log(error);
-    });
-    
+loadWetherAndOutput(703448);
 
 
 
